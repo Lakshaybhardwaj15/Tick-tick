f2ba8d96df5220acbe8420dce23889a4fa4ce97a
lakshay <bhardwajlakshay238@gmail.com>
2024-09-27 12:38:34 +0530

    hi

--- backend/routes/dateRoutes.js
+++
@@ -1,9 +0,0 @@
-// /backend/routes/datesRoutes.js
-const express = require('express');
-const { saveRecurringDate, getRecurringDates } = require('../controllers/datesController');
-const router = express.Router();
-
-router.post('/recurring-dates', saveRecurringDate);
-router.get('/recurring-dates', getRecurringDates);
-
-module.exports = router;
